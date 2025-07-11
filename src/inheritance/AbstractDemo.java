9e9355b065392a0f85e425578c1f906bb1ac2785
Nguyen Lan Anh <nguyen.thi.lan.anh@sun-asterisk.com>
2025-07-11 08:24:53 +0700

    Update file

--- src/inheritance/AbstractDemo.java
+++ src/inheritance/AbstractDemo.java
@@ -1,5 +1,32 @@
 package inheritance;
 
-public class AbstractDemo {
+abstract class VehicleAbstract {
+    String name;
+    String color;
+
+    abstract void speed();
+
+    public String display() {
+        return "Vehicle: " + name + ", Color: " + color;
+    }
+}
+
+abstract class CarAbstract extends VehicleAbstract{
+    String producer = "Honda";
+
+    @Override
+    public String display() {
+        return super.display() + ", Producer: " + producer;
+    }
+
+    public static void main(String[] args) {
+        CarAbstract car = new CarAbstract() {
+            void speed() {}
+        };
+        car.name = "City";
+        car.color = "Red";
+        car.speed();
+        System.out.println(car.display());
+    }
 
 }
